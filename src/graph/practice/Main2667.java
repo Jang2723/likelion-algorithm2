ff23d6cda4ccbc67f5379d73125e37252a956492
Jang2723 <jangsb46@gmail.com>
2024-03-12 09:07:55 +0900

    Graph - Main2667 update

--- src/graph/practice/Main2667.java
+++ src/graph/practice/Main2667.java
@@ -3,7 +3,7 @@ package graph.practice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 // https://www.acmicpc.net/problem/2667 델타 탐색
 public class Main2667 {
@@ -22,19 +22,52 @@ public class Main2667 {
                 char col = line.charAt(j);
                 map[i][j] = col - '0';
             }
-//            System.out.println(Arrays.toString(map[i]));
         }
 
-        // 모든 집이 잇을 수 있는 지도의 칸을 반복해서 방문하는데,
-            // 만약 해당하는 칸에 집이 있을 경우
-            // 해당 칸을 시작점으로 BFS 또는 DFS 한다.
-            // 그리고 그 과정에서 방문할 수 있었던 집의 갯수를 반환하고
-            // 해당하는 킨을 방문하면서 0으로 바꿔준다.
+        // 모든 집이 있을 수 있는 지도의 칸을 반복해서 방문하는데,
+        List<Integer> counts = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 만약 해당하는 칸에 집이 있을 경우,
+                if (map[i][j] == 1) {
+                    // 해당 칸을 시작점으로 BFS 또는 DFS를 한다.
+                    // 그리고 그 과정에서 방문할 수 있었던 집의 갯수를 반환한다.
+                    counts.add(bfs(i, j));
+                }
+            }
+        }
     }
 
-    // Y, X를 시작점으로하고 BFS를 하고 최종적으로 방문한 집의 갯수를
+    // y, x를 시작점으로 BFS를 하고 최종적으로 방문한 집의 갯수를
     // 반환하는 메서드
     private static int bfs(int y, int x) {
-        return 1;
+        Queue<int[]> toVisit = new LinkedList<>();
+        toVisit.offer(new int[]{y, x});
+        // 집을 방문하면서 0으로 바꿔준다.
+        map[y][x] = 0;
+        int count = 0;
+        while (!toVisit.isEmpty()) {
+            int[] now = toVisit.poll();
+            // 방문 할때마다 값을 늘려준다.
+            count++;
+            for (int i = 0; i < 4; i++) {
+                int nextY = now[0] + dy[i];
+                int nextX = now[1] + dx[i];
+                if (
+                        checkBounds(nextY, nextX) &&
+                                map[nextY][nextX] == 1
+                ) {
+                    map[nextY][nextX] = 0;
+                    toVisit.offer(new int[]{nextY, nextX});
+                }
+            }
+        }
+
+        return count;
     }
+
+    private static boolean checkBounds(int y, int x) {
+        return -1 < y && y < n && -1 < x && x < n;
+    }
+
 }
